f11097bd980a1e6c93dc3f34b91b226b03ea7573
Fuoad <fuoad@outlook.com>
2021-10-07 19:00:28 -0400

    fixed some errors

--- Lab3.sql
+++ Lab3.sql
@@ -1,21 +1,23 @@
 CREATE TABLE supervises (
-    employee_id INTEGER REFERENCES employee(employee_id)
+    employee_id INTEGER,
+    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
 );
 
 CREATE TABLE branch ( 
     branch_no INTEGER PRIMARY KEY,
-    employee_id INTEGER REFERENCES employee(employee_id)
+    employee_id INTEGER,
     address VARCHAR(20) NOT NULL,
     bank_name VARCHAR(20) NOT NULL,
     phone_no NUMBER(9) NOT NULL,
-    FOREIGN KEY (employee_id)
+    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
 );
 
---CREATE TABLE has (
---    branch_no INTEGER REFERENCES branch(branch_no),
---    department_no INTEGER REFERENCES department(department_no)
---    FOREIGN KEY (branch_no, department_no)
---);
+CREATE TABLE has (
+   branch_no INTEGER,
+   department_no INTEGER,
+   FOREIGN KEY (branch_no) REFERENCES branch(branch_no),
+   FOREIGN KEY (department_no) REFERENCES department(department_no)
+);
 
 
 CREATE TABLE customer(
@@ -34,13 +36,14 @@ CREATE TABLE account(
 ); 
 
 CREATE TABLE savings_account(
-    interest FLOAT, 
+    interest FLOAT,
+    account_no INTEGER,
     FOREIGN KEY (account_no) REFERENCES account(account_no)
 ); 
 
 CREATE TABLE transaction(
     transaction_no INTEGER PRIMARY KEY, NOT NULL, 
-    amount FLOAT, 
+    amount FLOAT 
 ); 
 
 CREATE TABLE employee(
@@ -53,9 +56,11 @@ CREATE TABLE employee(
 CREATE TABLE loan(
     loan_no INTEGER PRIMARY KEY NOT NULL,
     intrest FLOAT,
-    FOREIGN KEY(emplyee_id)  REFERENCES empolyee(employee_id)
+    FOREIGN KEY(emplyee_id)  REFERENCES employee(employee_id)
 ); 
 
-CREATE TABLE department(); 
->>>>>>> 108f85364b760f6ee77c4a13c28ae440c670ee01
-
+CREATE TABLE department(
+    department_no INTEGER PRIMARY KEY,
+    employee_id INTEGER REFERENCES employee(employee_id),
+    FOREIGN KEY (employee_id) 
+); 
